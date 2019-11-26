fdd21fe7aefccf2f9d985ffc616676aba2be3654
RomaChel <romanchelombitko@gmail.com>
2019-11-26 22:03:23 +0200

    added getExistingIssue

---
+++ src/test/java/JiraapiTest.java
@@ -0,0 +1,30 @@
+import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.hamcrest.text.MatchesPattern;
+import org.testng.annotations.Test;
+
+import java.util.regex.Pattern;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class JiraapiTest {
+
+    @Test
+    public void getExistingIssue() {
+
+        Response response =
+                given().
+                        auth().preemptive().basic("Roman_Chelombitko", "Roman_Chelombitko").
+                        when().
+                        get("http://jira.hillel.it/rest/api/2/issue/WEBINAR-8887").
+                        then().
+                        extract().response();
+
+        assertEquals(200, response.statusCode());
+        assertEquals("WEBINAR-8887", response.path("key"));
+        final Matcher<String> matcher = new MatchesPattern(Pattern.compile("[A-Z]+\\-[0-9]+"));
+        assertTrue(matcher.matches("WEBINAR-8887"));
+    }
+}
